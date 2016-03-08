6a56524d8a39455a3ff143a693066de856c3a83e
Sasikala-Ravichandran <passion.rails@gmail.com>
2016-03-08 09:44:47 -0500

    Fixed the indentations

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,53 @@
+class CommentsController < ApplicationController
+
+  def index
+    post = Post.find(params[:post_id])
+    @comments = post.comments
+	end
+
+	def show
+    post = Post.find(params[:post_id])
+    @comment = post.comments.find(params[:id])
+  end
+
+	def new
+    post = Post.find(params[:post_id])
+    @comment = post.comments.build
+	end
+
+	def create
+    post = Post.find(params[:post_id])
+    @comment = post.comments.new(comment_params)
+    if @comment.save
+      redirect_to([@comment.post, @comment])
+    else
+      render action: "new"
+    end
+  end
+  
+  def edit
+    post = Post.find(params[:post_id])
+    @comment = post.comments.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:post_id])
+    @comment = post.comments.find(params[:id])@comment.update(comment_params)
+    redirect_to([@comment.post, @comment])
+  end
+	
+  def destroy
+    post = Post.find(params[:post_id])
+    @comment = post.comments.find(params[:id])
+    @comment.destroy
+    flash.notice = "comment is deleted"
+    redirect_to post
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+    
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+    @comment.post_id = @post.id
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:name, :title, :content)
+    end
+
+end
